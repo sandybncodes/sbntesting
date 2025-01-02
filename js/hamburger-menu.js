30d244f08af1b35be5610cbd7167a0a2e502ab84
sandubn <newemail@example.com>
2025-01-02 17:52:53 +0200

    updated navbar behavior

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,18 +1,26 @@
 function toggleMenu() {
     const navbarMenu = document.getElementById("navbar-menu");
-    if (navbarMenu.style.display === "block") {
+    const computedStyle = window.getComputedStyle(navbarMenu);
+
+    if (computedStyle.display === "block") {
         navbarMenu.style.display = "none";
     } else {
         navbarMenu.style.display = "block";
     }
 }
 
-// Reset the navbar display on resize
-window.addEventListener('resize', () => {
+// Function to set the navbar display based on window size
+function setNavbarDisplay() {
     const navbarMenu = document.getElementById("navbar-menu");
     if (window.innerWidth > 800) {
         navbarMenu.style.display = "flex"; // Ensure nav bar is shown
     } else {
         navbarMenu.style.display = "none"; // Hide nav bar for small screens
     }
-});
+}
+
+// Set the navbar display on initial load
+window.addEventListener('load', setNavbarDisplay);
+
+// Reset the navbar display on resize
+window.addEventListener('resize', setNavbarDisplay);
